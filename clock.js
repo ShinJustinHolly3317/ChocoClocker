3524eee429c64362ab4fe3fa8f8ac3ab5deabaa6
justin kao <justinkao@justindeMBP.Dlink>
2023-07-11 22:32:54 +0800

    init
    init
    
    add gitignore
    
    remove node_modules
    
    remove node_modules
    
    remove unneccessary
    
    update gitignore
    
    update clock.js
    
    add env

---
+++ clock.js
@@ -0,0 +1,35 @@
+import * as dotenv from 'dotenv'
+import puppeteer from 'puppeteer'
+import { localCookies } from './local-cookies.js'
+dotenv.config();
+
+;(async () => {
+  const browser = await puppeteer.launch({ headless: false })
+  const page = await browser.newPage()
+  await page.setCookie(...localCookies)
+
+  await page.goto('https://bsignin.104.com.tw/login')
+
+  // Typing account info
+  await page.waitForSelector('input[data-qa-id="loginUserName"]')
+  await page.type('input[data-qa-id="loginUserName"]', process.env.account)
+  await page.waitForSelector('input[data-qa-id="loginPassword"]')
+  await page.type('input[data-qa-id="loginPassword"]', process.env.password)
+
+  // log in
+  const loginBtnSelector = '[data-qa-id="loginButton"]'
+  await page.waitForSelector(loginBtnSelector)
+  await page.click(loginBtnSelector)
+  await page.waitForNavigation()
+
+  // go to secretary page
+  await page.goto('https://pro.104.com.tw/psc2')
+  const gotomainSelector = '[class="btn btn-white btn-lg btn-block"]'
+  await page.waitForSelector(gotomainSelector)
+  let source = await page.content()
+
+  await page.click(gotomainSelector)
+  await page.waitForNavigation()
+
+  await browser.close()
+})()

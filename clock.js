ec2a3ec928946d025e3732d19414e6ef78c6f316
ShinJustinHolly3317 <83599048+ShinJustinHolly3317@users.noreply.github.com>
2024-04-25 14:56:19 +0800

    feat: wait for correct clock date time been rendered

--- clock.js
+++ clock.js
@@ -3,6 +3,16 @@ import puppeteer from 'puppeteer'
 import { localCookies } from './local-cookies.js'
 dotenv.config();
 
+// recurisve find target
+const avoidTarget = async (asyncFn, target) => {
+  const val = await asyncFn();
+  if (val !== target) {
+    return true;
+  } else {
+    return avoidTarget(fn, target);
+  }
+}
+  
 ;(async () => {
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
@@ -45,6 +55,16 @@ dotenv.config();
     // 等待特定的 div 元素出现
     const divElement = await page.waitForSelector(selector, { timeout: 300000 }); 
 
+    // wait for clock-in botton div
+    const divElement = await page.waitForSelector(selector, { timeout: 10000 }); 
+    // wait for clock-in time updated, cause default is 1970.1.1, sometimes will clock with that time before the datetime updated.
+    const dateEle = await page.waitForSelector('.PSC-ClockIn .-date', { timeout: 10000 });
+    avoidTarget(async () => {
+      const dateText = await page.evaluate(element => element.textContent, dateEle);
+      console.log('dateText', dateText);
+      return dateText.split('.')[0];
+    }, 1970)
+    
     const divHTML = await page.evaluate(div => div.outerHTML, divElement);
     console.log('div 元素的 HTML：', divHTML);
     page.on('console', message =>

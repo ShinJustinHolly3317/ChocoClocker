89b2db8f8e05b77c8f3ebf82f24c4df26fcdc04e
ShinJustinHolly3317 <83599048+ShinJustinHolly3317@users.noreply.github.com>
2024-05-21 10:46:11 +0800

    feat: use fetch to call clockin api instead of using puppeteer button click

--- clock.js
+++ clock.js
@@ -13,7 +13,14 @@ const avoidTarget = async (asyncFn, target) => {
     return avoidTarget(asyncFn, target);
   }
 }
-  
+
+const randInt = (int) => {
+  const randint = Math.ceil(Math.random() * int)
+  console.log(`randint: ${randint}`);
+  return randint
+}
+const sleepByMins = (mins) => new Promise((res) => setTimeout(res, mins * 1000 * 60))
+
 ;(async () => {
   console.log(`Clocker started at ${new Date()}`);
   const browser = await puppeteer.launch({ headless: false })
@@ -38,51 +45,52 @@ const avoidTarget = async (asyncFn, target) => {
   // go to secretary page
   await page.goto('https://pro.104.com.tw/psc2')
   await page.content()
-  
-  try {
-    const currentTime = new Date();
-    const currentHour = currentTime.getHours();
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
 
-    let selector = '';
+  let selector = '';
 
-    // 由時間做判斷
-    if (currentHour < 12) {
-      selector = '.PSC-ClockIn.morning'; 
-    } else if (currentHour >= 12 && currentHour < 18) {
-      selector = '.PSC-ClockIn.noon'; 
-    } else {
-      selector = '.PSC-ClockIn.night';
-    }
-
-    // 等待特定的 div 元素出现
-    const divElement = await page.waitForSelector(selector, { timeout: 300000 }); 
-
-    // wait for clock-in botton div
-    const divElement = await page.waitForSelector(selector, { timeout: 100000 }); 
-    // wait for clock-in time updated, cause default is 1970.1.1, sometimes will clock with that time before the datetime updated.
-    const dateEle = await page.waitForSelector('.PSC-ClockIn .-date', { timeout: 100000 });
-    avoidTarget(async () => {
-      const dateText = await page.evaluate(element => element.textContent, dateEle);
-      console.log('dateText', dateText);
-      return dateText.split('.')[0];
-    }, '1970')
-    
-    const divHTML = await page.evaluate(div => div.outerHTML, divElement);
-    console.log('div 元素的 HTML：', divHTML);
-    page.on('console', message =>
-    console.log(`log: $${message.text()}`))
-    await page.evaluate(async () => {
-      const button = document.querySelector('.right-side > span.btn');
-      if (button) {
-
-        button.click();
-        console.log('已點擊');
-      } else {
-        console.log('未找到按钮');
-      }
-    });
-  } catch (error) {
-    console.error('未找到特定的 div：', error);
+  // 由時間做判斷
+  if (currentHour < 12) {
+    selector = '.PSC-ClockIn.morning'; 
+  } else if (currentHour >= 12 && currentHour < 18) {
+    selector = '.PSC-ClockIn.noon'; 
+  } else {
+    selector = '.PSC-ClockIn.night';
   }
-  await browser.close()
+
+  // wait for clock-in botton div
+  await page.waitForSelector(selector, { timeout: 1000000 }); 
+  // wait for clock-in time updated, cause default is 1970.1.1, sometimes will clock with that time before the datetime updated.
+  await page.waitForSelector('.PSC-ClockIn .-date', { timeout: 1000000 });
+  const cookie = (await page.cookies()).map(cookieObj => `${cookieObj.name}=${cookieObj.value}`).join('; ');
+
+  await sleepByMins(randInt(10))
+
+  console.log(cookie);
+  fetch("https://pro.104.com.tw/psc2/api/f0400/newClockin", {
+    "headers": {
+      "accept": "application/json",
+      "accept-language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7,zh-CN;q=0.6",
+      "content-type": "application/json",
+      "priority": "u=1, i",
+      "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "x-request": "JSON",
+      "x-requested-with": "XMLHttpRequest",
+      "cookie": cookie,
+      "Referer": "https://pro.104.com.tw/psc2",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": "",
+    "method": "POST"
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error))
+  .finally(() => browser.close())
 })()

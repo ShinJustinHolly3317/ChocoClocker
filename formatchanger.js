18c20ae1e182497be0fa3f46c9ab6160a665d605
SimonAllen <simonallen19930901@gmail.com>
2025-02-12 21:24:41 +0800

    refactor: remove unnecessary fs import

--- formatchanger.js
+++ formatchanger.js
@@ -1,28 +1,23 @@
-import * as fs from 'fs';
-
 const cookies = process.argv[2];
 
-const cookiesObj = cookies
-  .split(';')
-  
-  .map(cookie => {
-    const cookieContent = cookie.split('=')
-    const cookieName = cookieContent[0].trim();
-    const cookieValue = cookieContent[1].trim();
-    return {
-      name: cookieName,
-      value: cookieValue,
-      domain: '.104.com.tw',
-      path: '/',
-      expires: -1,
-      size: 276,
-      httpOnly: false,
-      secure: false,
-      session: true,
-      sameParty: false,
-      sourceScheme: 'Secure',
-      sourcePort: 443
-    }
-  });
+const cookiesObj = cookies.split(";").map((cookie) => {
+  const cookieContent = cookie.split("=");
+  const cookieName = cookieContent[0].trim();
+  const cookieValue = cookieContent[1].trim();
+  return {
+    name: cookieName,
+    value: cookieValue,
+    domain: ".104.com.tw",
+    path: "/",
+    expires: -1,
+    size: 276,
+    httpOnly: false,
+    secure: false,
+    session: true,
+    sameParty: false,
+    sourceScheme: "Secure",
+    sourcePort: 443,
+  };
+});
 
-console.log(`export const localCookies = ${JSON.stringify(cookiesObj)}`)
+console.log(`export const localCookies = ${JSON.stringify(cookiesObj)}`);
